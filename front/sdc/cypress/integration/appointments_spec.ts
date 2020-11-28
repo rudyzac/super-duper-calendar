09c3921dc20946b3235e5b14166f111961ef73ba
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2020-11-28 01:03:27 +0100

    F: deleted /e2e folder

--- front/sdc/cypress/integration/appointments_spec.ts
+++ front/sdc/cypress/integration/appointments_spec.ts
@@ -1,6 +1,6 @@
-const { createYield } = require("typescript")
 describe('Appointments', () => {
-    it("creates an appointment and shows a confirmation message", () => {
-        
-    })
-})
+    it('creates an appointment and shows a confirmation message', () => {
+        // TODO: set base URL in cypress.json
+        cy.visit('http://localhost:4200/');
+    });
+});
