ac43540a8cbf5b929f4ad172b72cc3c099da0af4
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2020-12-30 13:04:39 +0100

    Front: registered FullCalendar with app.module

--- front/sdc/src/app/app.module.ts
+++ front/sdc/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 import { AppComponent } from './app.component';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
