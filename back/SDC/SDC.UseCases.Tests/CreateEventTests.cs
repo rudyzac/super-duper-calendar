6deb0f58f32aeb9fba7888363c015c5e3b3fb2c7
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2021-01-08 01:14:22 +0100

    Back: CreateEventTests scaffolding

---
+++ back/SDC/SDC.UseCases.Tests/CreateEventTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace SDC.UseCases.Tests
+{
+    public class CreateEventTests
+    {
+        [Fact]
+        public void ShouldCreateAnEvent()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
