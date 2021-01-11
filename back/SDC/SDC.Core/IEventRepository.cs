fe6536245a134c46b2943d1e0904d6c9e6fdf454
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2021-01-12 00:18:13 +0100

    Back: installed Moq, FluentAssertions; test for event creation in place.

--- back/SDC/SDC.Core/IEventRepository.cs
+++ back/SDC/SDC.Core/IEventRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SDC.Core
 {
-    interface IRepository
+    public interface IEventRepository
     {
+        
     }
 }
--- back/SDC/SDC.UseCases.Tests/CreateEventTests.cs
+++ back/SDC/SDC.UseCases.Tests/CreateEventTests.cs
@@ -1,5 +1,7 @@
-using System;
 using Xunit;
+using Moq;
+using SDC.Core;
+using FluentAssertions;
 
 namespace SDC.UseCases.Tests
 {
@@ -9,10 +11,15 @@ namespace SDC.UseCases.Tests
         public void ShouldCreateAnEvent()
         {
             // ARRANGE
+            var expected = new EventEntity();
+            var stubRepository = new Mock<IEventRepository>();
+            var useCase = new CreateEventUseCase(stubRepository);
 
             // ACT
+            var actual = useCase.Handle();
 
             // ASSERT
+            actual.Should().Be(expected);
         }
     }
 }
