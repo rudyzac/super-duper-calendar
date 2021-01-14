8a8ad986c048b396f2ac36195e5413357ca6a98c
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2021-01-14 23:43:34 +0100

    Back: test folder hierarchy

---
+++ back/SDC/SDC.UseCases.Tests/ProgressSuite/Integration/GetEventTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDC.UseCases.Tests.ProgressSuite.Integration
+{
+    public class GetEventTest
+    {
+        [Fact]
+        public void ShouldGetTheRequestedEvent()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+    }
+}
