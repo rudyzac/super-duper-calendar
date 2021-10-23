91c0214a70d0c0f9fd5bfbfa85cdbd768bde361e
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2021-10-23 12:52:16 +0200

    Dropped Angular in favour of React

--- front/sdc/cypress/integration/appointments_spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Appointments', () => {
-    it('creates an appointment and shows a confirmation message', () => {
-        // TODO: set base URL in cypress.json
-        cy.visit('http://localhost:4200/');
-        cy.get('[data-cy-input-title]').type('Groceries');
-        cy.get('[data-cy-input-date]').type('01-02-2021');
-        cy.get('[data-cy-confirmation-msg]').should('eq', '01-02-2021: Groceries');
-    });
-});
--- front/sdc/cypress/integration/homepage_spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Homepage', () => {
-    it('shows a calendar', () => {
-        cy.visit('http://localhost:4200/');
-        cy.get('[data-cy=calendar]');
-    })
-})
